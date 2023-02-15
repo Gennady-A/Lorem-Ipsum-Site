5d543a55c41c91eb7183c417ebfdff6f62739d07
GennadyA <gennadyaleynikov@gmail.com>
2023-02-15 23:30:54 +0300

    add generator from ru.lipsum.com

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+import requests
+from bs4 import BeautifulSoup
 # Create your views here.
 
 
@@ -10,6 +11,35 @@ def lorem_ipsum(request):
     return render(request, 'main\lorem_ipsum.html')
 
 def rusLI(request):
+    if request.method == 'POST':
+        url = 'http://www.lipsum.com/feed/xml'
+        data = request.POST
+        print(data)
+        if ('amount' in data):
+            amount = data['amount']
+        else:
+            amount = 10
+
+        if ('what' in data):
+            what = data['what']
+        else:
+            what = 'words'
+
+        if ('start' in data):
+            start = 'yes'
+        else:
+            start = 'no'
+
+        ans = ''
+
+        url = f'https://www.lipsum.com/feed/xml?amount={amount}&what={what}&start={start}'
+        print(url)
+        req = requests.get(url)
+        page = BeautifulSoup(req.text, "lxml")
+        ans = (page.find('lipsum')).text
+        print(ans)
+
+        return render(request, 'main\RusTemp.html', {'generatorText': ans})
     return render(request, 'main\RusTemp.html')
 
 def engLI(request):
